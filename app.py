5c765c686ee5ad70a6330a4ce2666d2f95aa5f23
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 13:46:40 -0500

    Change

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template, request
-import pandas as pd
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
    return "<h1>Hi</h1>"
-
-
-
-   
-
-
-
 
 if __name__ == '__main__':
   app.run()
