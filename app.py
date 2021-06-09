91dd91cf82622308d09baca6ea0d8891cb2428c7
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 17:01:53 -0500

    test1

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import csv
+
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route('/', methods=['GET', 'POST'])
+def index():
    return render_template("index.html")
+
+@app.route('/data', methods=['GET', 'POST'])
+def data():
+   if request.method == 'POST':
+      f = request.form['csvfile']
+      data = []
+      with open(f) as file:
+         csvfile csv.reader(file)
+         for row in csvfile:
+            data.append(row)  
+      return render_template('data.html', data=data)
+
 
 @app.route('/page1')
 def page1():
