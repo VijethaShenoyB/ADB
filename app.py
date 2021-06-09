c348676c1222119b80f704c37acdadd3b9654967
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 13:34:28 -0500

    test

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ password = 'Vijushenoy96'
 driver= '{ODBC Driver 17 for SQL Server}'
 
 
-cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-cursor = cnxn.cursor()
+
 
 
 @app.route('/')
