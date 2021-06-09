1108a26f1ef7be1d46b642ea41046d0df07c743e
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 15:55:27 -0500

    test

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def hello():
 
 @app.route('/page1')
 def page1():
-   return render_template("page1.html")
+   return render_template("./page1.html")
 
 @app.route('/page2')
 def page2():
-   return render_template("page2.html")
+   return render_template("./page2.html")
  
 if __name__ == '__main__':
   app.run()
