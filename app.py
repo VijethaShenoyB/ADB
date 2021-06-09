29352202b0173d9536b1583592289cccb48a899d
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 14:11:59 -0500

    test

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/')
 def hello():
    return "<h1>Hi</h1>"
 
-server = 'shenoyserver.database.windows.net'
-database = 'ShenoyDB'
-username = 'vijethashenoy'
-password = 'Vijushenoy96'
-driver= '{ODBC Driver 17 for SQL Server}'
+@app.route('/page1')
+def hello():
+   return render_template("page1.html")
 
-cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
-cursor = cnxn.cursor()
+@app.route('/page2')
+def hello():
+   return render_template("page2.html")
  
-
 if __name__ == '__main__':
   app.run()
