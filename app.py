f84bc278b47309ca630b0a2ca2b22f09c0e16e20
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 16:17:34 -0500

    test

--- app.py
+++ app.py
@@ -1,6 +1,25 @@
 from flask import Flask, render_template, request
+import pyodbc
 
 app = Flask(__name__)
+
+server = 'shenoyserver.database.windows.net'
+database = 'ShenoyDB'
+username = 'vijethashenoy'
+password = 'Vijushenoy96'
+driver= '{ODBC Driver 17 for SQL Server}'
+
+cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+cursor = cnxn.cursor()
+
+#display image for given name
+@app.route("/", methods=['GET','POST'])
+def display():
+    getname = 'Mars'
+    cursor.execute("select Picture from datab where Name = 'Mars';")
+    getimage = cursor.fetchall()
+    return render_template('index.html', setimage = "mars.jpg")
+
 
 @app.route('/')
 def hello():
