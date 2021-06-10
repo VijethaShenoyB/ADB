3bda81c9865f37ccee72f7b4834c2801e91e9e5d
Arjun Kalladka Shenoy <arjunshenoy@Arjuns-MBP.attlocal.net>
2021-06-09 23:47:25 -0500

    other

--- app.py
+++ app.py
@@ -1,33 +1,87 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
 import csv
-
-
+import numpy as np
+import os
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-   return render_template("index.html")
-
-@app.route('/data', methods=['GET', 'POST'])
-def data():
-   if request.method == 'POST':
-      f = request.form['csvfile'] 
-      data = []
-      with open(f) as file:
-         csvfile = csv.reader(file)
-         for row in csvfile:
-            data.append(row)
-         print(type(data))
-      return render_template('data.html', data=data)
-
-
-@app.route('/page1')
-def page1():
-   return render_template("./page1.html")
-
-@app.route('/page2')
-def page2():
-   return render_template("./page2.html")
+path="./people.csv"
+tempPath="./new.csv"
  
-if __name__ == '__main__':
-  app.run()
+fieldnames=['Name','State','Salary','Grade','Room','Telnum','Picture','Keywords']
+
+df = pd.read_csv('people.csv')
+df1=df.replace(np.nan,"",regex=True)
+
+data = df1.values.tolist()
+
+@app.route('/', methods=["POST","GET"])
+def hello():
+	return render_template('index.html')
+
+@app.route('/homepage', methods=["POST","GET"])
+def home():
+	return render_template('index.html')
+
+@app.route('/alldata',methods=["POST","GET"])
+def search():	
+	df = pd.read_csv('people.csv')
+	df1=df.replace(np.nan,"",regex=True)
+	data = df1.values.tolist()
+	return render_template('alldata.html',dict=data)
+
+@app.route('/takedata',methods=["POST","GET"])
+def searchdata():
+	df = pd.read_csv('people.csv')
+	df1=df.replace(np.nan,"",regex=True)
+	data = df1.values.tolist()
+	name = request.form.get("SearchBar")
+	return render_template('search.html',dict=data, name=name)
+	
+
+@app.route('/saldata',methods=["POST","GET"])
+def saldata():
+	df = pd.read_csv('people.csv')
+	df1=df.replace(np.nan,"",regex=True)
+	data = df1.values.tolist()
+	people = []
+	sal = request.form.get("salBar")
+	sal = float(sal)
+	for items in data:
+		salary = 0
+		if(items[2] != "" and items[2] != " "):
+			salary = float(items[2])
+		if (salary < sal):
+			people.append(items)
+	return render_template('salbaseddata.html',dict=people, sal=sal)
+
+@app.route('/update',methods=["POST","GET"])
+def updatedata():	
+	name = request.form.get("name")
+	print(name)
+	state = request.form.get("state")
+	salary = request.form.get("salary")
+	print(salary)
+	grade = request.form.get("grade")
+	room = request.form.get("room")
+	telnum = request.form.get("telnum")
+	keywords = request.form.get("keywords")
+	with open(tempPath, mode='w') as csv_file:
+		linewriter=csv.writer(csv_file)
+		mywriter=csv.DictWriter(csv_file,fieldnames=fieldnames)
+		mywriter.writeheader()
+		with open(path, mode='r') as csv_file:
+			myreader = csv.DictReader(csv_file)
+			for row in myreader:
+				if row['Name']==name:
+					print(row)
+					if(request.form['update'] == 'Update'):
+						linewriter.writerow([name,state,salary,grade,room,telnum,row['Picture'],keywords])
+					else:
+						continue
+				else:
+					mywriter.writerow(row)
+	os.remove(path)
+	os.rename(tempPath,path)
+
+	return render_template('index.html')
